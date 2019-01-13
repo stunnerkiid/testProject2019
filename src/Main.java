a8a729ecea72df79342e291bed64bc383137086d
stunnerkiid <vkarwee@gmail.com>
2019-01-13 12:28:38 -0600

    from original laptop

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        printPush();
     }
+
+
+    static public void printPush() {
+        System.out.print("I pushed this message from my regular laptop");
+
+
+    }
+
 }
+
+
+
+
+
+
+
